061b0d2f67439da0a382011f73d935652598885e
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2017-08-21 10:12:33 +0200

    Remove unnecessary dll extension from AssemblyName since it will be added anyway

--- JShellOverlayIcon/build.handler.net2.x64.cmd
+++ JShellOverlayIcon/build.handler.net2.x64.cmd
@@ -1,1 +1,1 @@
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" JShellOverlayIconHandler.csproj /p:TargetFrameworkVersion=v2.0;Platform=x64;AssemblyName=JShellOverlayIconHandler.net2.x64.dll /tv:15.0
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" JShellOverlayIconHandler.csproj /p:TargetFrameworkVersion=v2.0;Platform=x64;AssemblyName=JShellOverlayIconHandler.net2.x64 /tv:15.0
--- JShellOverlayIcon/build.handler.net4.x86.cmd
+++ JShellOverlayIcon/build.handler.net4.x86.cmd
@@ -1,1 +1,1 @@
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" JShellOverlayIconHandler.csproj /p:TargetFrameworkVersion=v4.0;Platform=x86;AssemblyName=JShellOverlayIconHandler.net4.x86.dll /tv:15.0
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" JShellOverlayIconHandler.csproj /p:TargetFrameworkVersion=v4.0;Platform=x86;AssemblyName=JShellOverlayIconHandler.net4.x86 /tv:15.0
